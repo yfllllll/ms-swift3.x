f9c450656ed6137acfafe4d51671c09c9984eda8
yfllllll <yanfeiliu@whu.edu.cn>
2025-01-04 18:42:48 +0800

    update

--- mytools/merge_and_quant.sh
+++ mytools/merge_and_quant.sh
@@ -8,7 +8,7 @@ OMP_NUM_THREADS=14 \
 CUDA_VISIBLE_DEVICES=0 \
 swift export \
     --model ${mergeed} \
-    --dataset output.jsonl \
+    --dataset ../output.jsonl \
     --quant_n_samples 128 \
     --quant_batch_size 1 \
     --max_length 2048 \
