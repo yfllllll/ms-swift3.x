6a6de848facff6e047293d8cbd527d39831c63a1
yfllllll <yanfeiliu@whu.edu.cn>
2025-01-04 17:22:51 +0800

    更新量化和计算检测指标文件

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+
+
 CUDA_VISIBLE_DEVICES=0 MAX_PIXELS=921600 swift sft \
   --model qwen/Qwen2-VL-7B-Instruct \
   --train_type lora \
--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,16 @@
-NPROC_PER_NODE=4 \
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+NPROC_PER_NODE=7 \
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 \
 MAX_PIXELS=921600 \
 swift sft \
-  --model qwen/Qwen2-VL-2B-Instruct \
+  --model qwen/Qwen2-VL-7B-Instruct \
   --eval_steps 3000 \
-  --dataloader_num_workers 1 \
-  --dataset dataset/fromVary_3.0/Tower_bigdata_train.jsonl \
-            dataset/fromVary_3.0/Tower_data_1_train.jsonl \
-  --deepspeed default-zero3 
+  --deepspeed zero3 \
+  --save_steps 3000 \
+  --num_train_epochs 1 \
+  --strict False \
+  --per_device_train_batch_size 1 \
+  --dataset output.jsonl
+
 
   
   
